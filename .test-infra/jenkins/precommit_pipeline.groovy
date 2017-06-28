46892dcffd24a0f54c7b074407e8702b10b9002d
Jason Kuster <jasonkuster@google.com>
2017-06-27 19:05:40 -0700

    start seeing what we get
    Signed-off-by: Jason Kuster <jasonkuster@google.com>

--- .test-infra/jenkins/precommit_pipeline.groovy
+++ .test-infra/jenkins/precommit_pipeline.groovy
@@ -5,7 +5,9 @@ PR_SHA = "origin/pr/${ghprbPullId}/head"
 try {
     stage('Build') {
         compileBuild = build job: 'beam_PreCommit_Build', parameters: [string(name: 'sha1', value: PR_SHA)]
+        echo string(compileBuild.BUILD_NUM)
+        echo string(compileBuild)
     }
 } catch (Exception e) {
-    echo e
+    echo string(e)
 }
