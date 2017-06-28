d65e2e5a07491825ffb63e5c899d405d71285242
Jason Kuster <jasonkuster@google.com>
2017-06-27 19:41:40 -0700

    test build
    Signed-off-by: Jason Kuster <jasonkuster@google.com>

--- .test-infra/jenkins/precommit_pipeline.groovy
+++ .test-infra/jenkins/precommit_pipeline.groovy
@@ -1,13 +1,14 @@
 #!groovy
 
-PR_SHA = "origin/pr/${ghprbPullId}/head"
+// PR_SHA = "origin/pr/${ghprbPullId}/head"
 
 try {
     stage('Build') {
-        compileBuild = build job: 'beam_PreCommit_Build', parameters: [string(name: 'sha1', value: PR_SHA)]
-        echo string(compileBuild.BUILD_NUM)
-        echo string(compileBuild)
+        compileBuild = build job: 'mb-merge-1' // , parameters: [string(name: 'sha1', value: PR_SHA)]
+        echo "" + compileBuild.env.BUILD_NUM
+        echo compileBuild.env
+        echo compileBuild
     }
 } catch (Exception e) {
-    echo string(e)
+    echo e.toString()
 }
