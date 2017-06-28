812bf42dc15b2cfbcf9f9864b2f3d83f7100bc6b
Jason Kuster <jasonkuster@google.com>
2017-06-27 19:50:28 -0700

    get number
    Signed-off-by: Jason Kuster <jasonkuster@google.com>

--- .test-infra/jenkins/precommit_pipeline.groovy
+++ .test-infra/jenkins/precommit_pipeline.groovy
@@ -1,13 +1,15 @@
 #!groovy
+import hudson.model.Result
 
 // PR_SHA = "origin/pr/${ghprbPullId}/head"
 
 try {
     stage('Build') {
         compileBuild = build job: 'mb-merge-1' // , parameters: [string(name: 'sha1', value: PR_SHA)]
-        echo "" + compileBuild.env.BUILD_NUM
-        echo compileBuild.env
-        echo compileBuild
+        if(compileBuild.getResult() == Result.SUCCESS.toString()) {
+            echo "" + compileBuild.getNumber()
+            echo compileBuild.getBuildVariables().get("BUILD_NUM")
+        }
     }
 } catch (Exception e) {
     echo e.toString()
