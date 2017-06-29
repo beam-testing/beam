23556a47ac8624eb5e28c9a5548b8fd27320a756
Jason Kuster <jasonkuster@google.com>
2017-06-28 23:52:50 -0700

    use commas
    Signed-off-by: Jason Kuster <jasonkuster@google.com>

--- .test-infra/jenkins/PreCommit_Pipeline.groovy
+++ .test-infra/jenkins/PreCommit_Pipeline.groovy
@@ -8,9 +8,9 @@ try {
         parallel (
             java: {
                 def javaBuild = build job: 'beam_PreCommit_Java_Build', parameters: [
-		    string(name: 'sha1', value: "origin/pr/${ghprbPullId}/head")
-		    string(name: 'ghprbGhRepository', value: "${ghprbGhRepository}")
-		    string(name: 'ghprbActualCommit', value: "${ghprbActualCommit}")
+		    string(name: 'sha1', value: "origin/pr/${ghprbPullId}/head"),
+		    string(name: 'ghprbGhRepository', value: "${ghprbGhRepository}"),
+		    string(name: 'ghprbActualCommit', value: "${ghprbActualCommit}"),
 		    string(name: 'ghprbPullId', value: "${ghprbPullId}")
 		]
                 if(javaBuild.getResult() == Result.SUCCESS.toString()) {
